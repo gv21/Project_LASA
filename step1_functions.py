5739f29338e31c0393883c9b5d4ba87599c70aeb
gv21 <gaelle.verdon@epfl.ch>
2024-06-09 16:54:32 +0200

    Update alla

--- step1_functions.py
+++ step1_functions.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from time_stitches import *
 
 def compute_velocity(x_t0, x_t1, t0, t1):
     velocity = (x_t1 - x_t0)/(t1-t0)
@@ -210,6 +211,24 @@ def remove_extreme_using_z_pos2(nh_rec, tw_rec, stds_nh, stds_tw, medians_nh, me
 
     return mask_selected_nh, mask_selected_tw
 
+def remove_extreme_using_z_pos_exception(nh_rec, tw_rec, stds_nh, stds_tw, medians_nh, medians_tw):
+    """Create a mask keeping all points which are between two extreme values"""
+    median_nh_z = medians_nh['Z.1']
+    median_tw_z = medians_tw['Z.1']
+    std_z_nh = stds_nh['Z.1']
+    std_z_tw = stds_tw['Z.1']
+
+    #A deviation from the main task (median) of 0.02m is considered as reasonable
+    nh_threshold_z_min= median_nh_z - max(0.03, std_z_nh)
+    nh_threshold_z_max= median_nh_z + max(0.03, std_z_nh)
+    tw_threshold_z_min= median_tw_z - max(0.03, std_z_tw)
+    tw_threshold_z_max= median_tw_z + max(0.03, std_z_tw)
+
+    mask_selected_nh = (nh_rec['Z.1']<nh_threshold_z_max) & (nh_rec['Z.1']>nh_threshold_z_min)
+    mask_selected_tw = (tw_rec['Z.1']<tw_threshold_z_max) & (tw_rec['Z.1']>tw_threshold_z_min)
+
+    return mask_selected_nh, mask_selected_tw
+
 #function to put a threshold on Y axis using the already x-selected signal
 def remove_extreme_using_y_pos(nh_rec, tw_rec, stds_nh, stds_tw, medians_nh, medians_tw):
     """Create a mask keeping all points which are between two extreme values"""
@@ -230,6 +249,25 @@ def remove_extreme_using_y_pos(nh_rec, tw_rec, stds_nh, stds_tw, medians_nh, med
     return mask_selected_nh, mask_selected_tw
 
 #function to put a threshold on Y axis using the already x-selected signal
+def remove_extreme_using_y_pos_exception(nh_rec, tw_rec, stds_nh, stds_tw, medians_nh, medians_tw):
+    """Create a mask keeping all points which are between two extreme values"""
+    median_nh_y = medians_nh['Y.1']
+    median_tw_y = medians_tw['Y.1']
+    std_y_nh = stds_nh['Y.1']
+    std_y_tw = stds_tw['Y.1']
+
+    #A deviation from the main task (median) of 0.015m is considered as reasonable
+    nh_threshold_y_min= median_nh_y - max(0.04, std_y_nh)
+    nh_threshold_y_max= median_nh_y + max(0.04, std_y_nh)
+    tw_threshold_y_min= median_tw_y - max(0.03, std_y_tw)
+    tw_threshold_y_max= median_tw_y + max(0.03, std_y_tw)
+
+    mask_selected_nh = (nh_rec['Y.1']<nh_threshold_y_max) & (nh_rec['Y.1']>nh_threshold_y_min)
+    mask_selected_tw = (tw_rec['Y.1']<tw_threshold_y_max) & (tw_rec['Y.1']>tw_threshold_y_min)
+
+    return mask_selected_nh, mask_selected_tw
+
+#function to put a threshold on Y axis using the already x-selected signal
 def remove_extreme_using_x_pos_on_selected_data(nh_rec, tw_rec, stds_nh, stds_tw, medians_nh, medians_tw):
     """Create a mask keeping all points which are between two extreme values"""
     median_nh_x = medians_nh['X.1']
@@ -242,6 +280,25 @@ def remove_extreme_using_x_pos_on_selected_data(nh_rec, tw_rec, stds_nh, stds_tw
     nh_threshold_x_max= median_nh_x + min(0.04, max(0.015, std_x_nh))
     tw_threshold_x_min= median_tw_x - min(0.04, max(0.015, std_x_tw))
     tw_threshold_x_max= median_tw_x + min(0.04, max(0.015, std_x_tw))
+
+    mask_selected_nh = (nh_rec['X.1']<nh_threshold_x_max) & (nh_rec['X.1']>nh_threshold_x_min)
+    mask_selected_tw = (tw_rec['X.1']<tw_threshold_x_max) & (tw_rec['X.1']>tw_threshold_x_min)
+
+    return mask_selected_nh, mask_selected_tw
+
+#function to put a threshold on Y axis using the already x-selected signal
+def remove_extreme_using_x_pos_on_selected_data_exception(nh_rec, tw_rec, stds_nh, stds_tw, medians_nh, medians_tw):
+    """Create a mask keeping all points which are between two extreme values"""
+    median_nh_x = medians_nh['X.1']
+    median_tw_x = medians_tw['X.1']
+    std_x_nh = stds_nh['X.1']
+    std_x_tw = stds_tw['X.1']
+
+    #A deviation from the main task (median) of 0.015m is considered as reasonable. Being far away than 0.04m is considered as not reasonable
+    nh_threshold_x_min= median_nh_x - min(0.06, max(0.03, std_x_nh))
+    nh_threshold_x_max= median_nh_x + min(0.06, max(0.03, std_x_nh))
+    tw_threshold_x_min= median_tw_x - min(0.04, max(0.02, std_x_tw))
+    tw_threshold_x_max= median_tw_x + min(0.04, max(0.02, std_x_tw))
 
     mask_selected_nh = (nh_rec['X.1']<nh_threshold_x_max) & (nh_rec['X.1']>nh_threshold_x_min)
     mask_selected_tw = (tw_rec['X.1']<tw_threshold_x_max) & (tw_rec['X.1']>tw_threshold_x_min)
@@ -330,3 +387,135 @@ def remove_extreme_using_z_pos2_sub19(nh_rec, tw_rec):
     mask_selected_nh = pd.concat([mask_selected_nh_part1, mask_selected_nh_part2], axis=0, ignore_index=True)
     mask_selected_tw = pd.concat([mask_selected_tw_part1, mask_selected_tw_part2], axis=0, ignore_index=True)
     return mask_selected_nh, mask_selected_tw
+
+
+def run_step1_per_subject (subject, i):
+    needle_holder_rec = pd.read_csv(f'Data/Sync_data/S_{subject}_NH_reconstructed.csv')
+    tweezers_rec = pd.read_csv(f'Data/Sync_data/S_{subject}_TW_reconstructed.csv')
+
+    medians_nh = (needle_holder_rec[['X.1', 'Y.1', 'Z.1']]).median()
+    medians_tw = (tweezers_rec[['X.1', 'Y.1', 'Z.1']]).median()
+
+    """X Position"""
+    selected_xpos_nh, selected_xpos_tw = remove_extreme_using_x_pos(needle_holder_rec, tweezers_rec, prop_nh=0.5, prop_tw=0.5)
+    time_event_xpos_nh, time_event_xpos_tw = compute_event_time_pos(full_segments_time[i], needle_holder_rec, tweezers_rec, selected_xpos_nh,
+                                                                selected_xpos_tw)
+
+    pairs_t_xpos_nh = compute_pairs_time(time_event_xpos_nh[0], time_event_xpos_nh[1])
+    pairs_t_xpos_tw = compute_pairs_time(time_event_xpos_tw[0], time_event_xpos_tw[1])
+
+    pairs_adjusted_t_xpos_nh = adjust_time_pair(needle_holder_rec, pairs_t_xpos_nh, medians_nh['X.1'], time_allowed = 6)
+    pairs_adjusted_t_xpos_tw = adjust_time_pair(tweezers_rec, pairs_t_xpos_tw, medians_tw['X.1'], time_allowed=6)
+
+    mask_extreme_xpos_event_removed_nh = remove_extreme(needle_holder_rec, pairs_adjusted_t_xpos_nh)
+    mask_extreme_xpos_event_removed_tw = remove_extreme(tweezers_rec, pairs_adjusted_t_xpos_tw)
+
+    """X Velocity"""
+    mask_min_max_nh, mask_min_max_tw = compute_mask_using_v(needle_holder_rec, tweezers_rec, smooth_window=60, prop_nh=0.4, prop_tw=0.4)
+    time_event_nh, time_event_tw = compute_event_time(full_segments_time[i], needle_holder_rec, tweezers_rec, mask_min_max_nh[0],  
+                                                            mask_min_max_nh[1], mask_min_max_tw[0], mask_min_max_tw[1])
+                
+    pairs_t_nh = compute_pairs_time(time_event_nh[0], time_event_nh[1])
+    pairs_t_tw = compute_pairs_time(time_event_tw[0], time_event_tw[1])
+
+    pairs_adjusted_t_nh = adjust_time_pair(needle_holder_rec, pairs_t_nh, medians_nh['X.1'], time_allowed = 6)
+    pairs_adjusted_t_tw = adjust_time_pair(tweezers_rec, pairs_t_tw, medians_tw['X.1'], time_allowed = 6)
+
+    mask_extreme_v_event_removed_nh = remove_extreme(needle_holder_rec, pairs_adjusted_t_nh)
+    mask_extreme_v_event_removed_tw = remove_extreme(tweezers_rec, pairs_adjusted_t_tw)
+    mask_extreme_v_glass_nh = remove_extreme(needle_holder_rec, pairs_adjusted_t_tw) #we don't want to keep event when the other tool is in the glass
+    mask_extreme_v_glass_tw = remove_extreme(tweezers_rec, pairs_adjusted_t_nh) #we don't want to keep event when the other tool is in the glass
+
+    """Select points"""
+    mask_glass_nh = mask_extreme_xpos_event_removed_nh & mask_extreme_v_event_removed_nh & selected_xpos_nh
+    mask_glass_tw = mask_extreme_xpos_event_removed_tw & mask_extreme_v_event_removed_tw & selected_xpos_tw
+    selected_points_nh = needle_holder_rec[mask_glass_nh & mask_extreme_v_glass_nh] #v is the most discriminative in this case compared to xpos
+    selected_points_tw = tweezers_rec[mask_glass_tw & mask_extreme_v_glass_tw] #v is the most discriminative in this case compared to xpos
+
+    #so far we excluded events where the tool is reaching the glass, we then want a more precise selection of points
+    """2nd selection based on z position"""
+    std_nh =  (selected_points_nh[['X.1', 'Y.1', 'Z.1']]).std()
+    std_tw =  (selected_points_tw[['X.1', 'Y.1', 'Z.1']]).std()
+    if (subject == 19): #subject 19 is treated separately, two different ref level of z position during the task 
+        selected_zpos_nh, selected_zpos_tw = remove_extreme_using_z_pos2_sub19(needle_holder_rec, tweezers_rec)
+    elif ((subject == 23) or (subject==27)):
+        selected_zpos_nh, selected_zpos_tw = remove_extreme_using_z_pos_exception(needle_holder_rec, tweezers_rec, std_nh, std_tw, medians_nh, medians_tw)
+    else: 
+        selected_zpos_nh, selected_zpos_tw = remove_extreme_using_z_pos2(needle_holder_rec, tweezers_rec, std_nh, std_tw, medians_nh, medians_tw)
+    
+    time_event_zpos_nh, time_event_zpos_tw = compute_event_time_pos(full_segments_time[i], needle_holder_rec, tweezers_rec, selected_zpos_nh,
+                                                                selected_zpos_tw)
+
+    pairs_t_zpos_nh = compute_pairs_time(time_event_zpos_nh[0], time_event_zpos_nh[1])
+    pairs_t_zpos_tw = compute_pairs_time(time_event_zpos_tw[0], time_event_zpos_tw[1])
+
+    pairs_adjusted_t_zpos_nh = adjust_time_pair(selected_points_nh, pairs_t_zpos_nh, medians_nh['Z.1'], time_allowed = 6)
+    pairs_adjusted_t_zpos_tw = adjust_time_pair(selected_points_tw, pairs_t_zpos_tw, medians_tw['Z.1'], time_allowed=6)
+
+    mask_extreme_zpos_event_removed_nh = remove_extreme(selected_points_nh, pairs_adjusted_t_zpos_nh)
+    mask_extreme_zpos_event_removed_tw = remove_extreme(selected_points_tw, pairs_adjusted_t_zpos_tw)
+
+    
+    selected_points_nh2 = selected_points_nh[mask_extreme_zpos_event_removed_nh]
+    selected_points_tw2 = selected_points_tw[mask_extreme_zpos_event_removed_tw]
+
+    """2nd selection based on y position"""
+    if ((subject == 23) or (subject==27)):
+        selected_ypos_nh, selected_ypos_tw = remove_extreme_using_y_pos_exception(needle_holder_rec, tweezers_rec, std_nh, std_tw, medians_nh, medians_tw)
+    else:
+        selected_ypos_nh, selected_ypos_tw = remove_extreme_using_y_pos(needle_holder_rec, tweezers_rec, std_nh, std_tw, medians_nh, medians_tw)
+    
+    time_event_ypos_nh, time_event_ypos_tw = compute_event_time_pos(full_segments_time[i], needle_holder_rec, tweezers_rec, selected_ypos_nh,
+                                                                selected_ypos_tw)
+
+    pairs_t_ypos_nh = compute_pairs_time(time_event_ypos_nh[0], time_event_ypos_nh[1])
+    pairs_t_ypos_tw = compute_pairs_time(time_event_ypos_tw[0], time_event_ypos_tw[1])
+
+    pairs_adjusted_t_ypos_nh = adjust_time_pair(selected_points_nh2, pairs_t_ypos_nh, medians_nh['Y.1'], time_allowed = 6)
+    pairs_adjusted_t_ypos_tw = adjust_time_pair(selected_points_tw2, pairs_t_ypos_tw, medians_tw['Y.1'], time_allowed=6)
+
+    mask_extreme_ypos_event_removed_nh = remove_extreme(selected_points_nh2, pairs_adjusted_t_ypos_nh)
+    mask_extreme_ypos_event_removed_tw = remove_extreme(selected_points_tw2, pairs_adjusted_t_ypos_tw)
+
+    
+    selected_points_nh3 = selected_points_nh2[mask_extreme_ypos_event_removed_nh]
+    selected_points_tw3 = selected_points_tw2[mask_extreme_ypos_event_removed_tw]
+
+    """2nd selection based on x position"""
+    #x position ref line of subject 1 is moving during the task, we need to treat it separately
+    """if (subject!=1):
+        selected_xpos2_nh, selected_xpos2_tw = remove_extreme_using_x_pos_on_selected_data_exception(needle_holder_rec, tweezers_rec, std_nh, std_tw, medians_nh, medians_tw)
+    else:
+        selected_xpos2_nh, selected_xpos2_tw  = remove_extreme_using_x_pos_on_selected_data_sub1(needle_holder_rec, tweezers_rec, selected_points_nh, selected_points_tw)
+    """
+    if (subject==1):
+        selected_xpos2_nh, selected_xpos2_tw  = remove_extreme_using_x_pos_on_selected_data_sub1(needle_holder_rec, tweezers_rec, selected_points_nh, selected_points_tw)
+    elif ((subject == 23) or (subject==27)):
+        selected_xpos2_nh, selected_xpos2_tw = remove_extreme_using_x_pos_on_selected_data_exception(needle_holder_rec, tweezers_rec, std_nh, std_tw, medians_nh, medians_tw)
+    else:
+        selected_xpos2_nh, selected_xpos2_tw = remove_extreme_using_x_pos_on_selected_data(needle_holder_rec, tweezers_rec, std_nh, std_tw, medians_nh, medians_tw)
+    
+    time_event_xpos2_nh, time_event_xpos2_tw = compute_event_time_pos(full_segments_time[i], needle_holder_rec, tweezers_rec, selected_xpos2_nh,
+                                                                selected_xpos2_tw)
+
+    pairs_t_xpos2_nh = compute_pairs_time(time_event_xpos2_nh[0], time_event_xpos2_nh[1])
+    pairs_t_xpos2_tw = compute_pairs_time(time_event_xpos2_tw[0], time_event_xpos2_tw[1])
+
+    pairs_adjusted_t_xpos2_nh = adjust_time_pair(selected_points_nh3, pairs_t_xpos2_nh, medians_nh['X.1'], time_allowed = 6)
+    pairs_adjusted_t_xpos2_tw = adjust_time_pair(selected_points_tw3, pairs_t_xpos2_tw, medians_tw['X.1'], time_allowed=6)
+
+    mask_extreme_xpos2_event_removed_nh = remove_extreme(selected_points_nh3, pairs_adjusted_t_xpos2_nh)
+    mask_extreme_xpos2_event_removed_tw = remove_extreme(selected_points_tw3, pairs_adjusted_t_xpos2_tw)
+
+    selected_points_nh4 = selected_points_nh3[mask_extreme_xpos2_event_removed_nh]
+    selected_points_tw4 = selected_points_tw3[mask_extreme_xpos2_event_removed_tw]
+
+    """Manual correction of incorrectly segmented points """
+    if (subject==1):
+        mask_tw = (selected_points_tw4['Time (Seconds)']>1275) & (selected_points_tw4['Time (Seconds)']<1287)
+        selected_points_tw4 = selected_points_tw4[~mask_tw]
+    if (subject==13):
+        mask_tw = (selected_points_tw4['Time (Seconds)']>679) & (selected_points_tw4['Time (Seconds)']<711.5)
+        selected_points_tw4 = selected_points_tw4[~mask_tw]
+    """end"""
+    return selected_points_nh4, selected_points_tw4
